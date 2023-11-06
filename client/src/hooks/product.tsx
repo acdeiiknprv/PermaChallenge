396eb56b56c92483d89dfc07b5612322619d2522
Kevin Picard <kevin.picard.au@gmail.com>
2023-11-07 09:24:09 +1100

    Add search

--- client/src/hooks/product.tsx
+++ client/src/hooks/product.tsx
@@ -1,27 +1,35 @@
 import { useState, useEffect } from 'react';
-import { getProducts } from '../services/getProduct';
+import { getProducts, getProductsBySearch } from '../services/getProduct';
 import { Product } from '../interfaces/product';
 
-export function useProducts() {
+export function useProducts(searchTerm: string) {
     const [products, setProducts] = useState<Product[]>([]);
-    const [refresh, setRefresh] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [refresh, setRefresh] = useState(false);
 
     useEffect(() => {
         async function fetchProducts() {
             setLoading(true);
-            const response = await getProducts();
-            if (response)
+            try {
+                let response;
+                if (searchTerm.trim()) {
+                    response = await getProductsBySearch(searchTerm);
+                } else {
+                    response = await getProducts();
+                }
                 setProducts(response.products);
+            } catch (error) {
+                console.error("Failed to fetch products:", error);
+            }
             setLoading(false);
         }
+
         fetchProducts();
-    }, [refresh]);
+    }, [searchTerm, refresh]);
 
     function handleRefresh() {
         setRefresh(prev => !prev);
     }
 
-    return { products, handleRefresh, loading };
+    return { products, loading, handleRefresh };
 }
-
