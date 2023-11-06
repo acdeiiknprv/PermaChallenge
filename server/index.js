3525bf98154dcbc7ec441fc772218022412fe64c
Kevin Picard <kevin.picard.au@gmail.com>
2023-11-06 13:46:58 +1100

    Server side files

---
+++ server/index.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const checkAuthenticated = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication token is missing' });
+  }
+
+  req.user = { token };
+  next();
+};
+
+app.get('/products', async (req, res) => {
+    try {
+        const response = await fetch(`https://dummyjson.com/products`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await fetch(`https://dummyjson.com/products/${id}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const response = await fetch('https://dummyjson.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body),
+    });
+
+    const data = await response.json();
+
+    if (data.token) {
+      res.json({ token: data.token });
+    } else {
+      res.status(401).json({ message: 'Authentication failed' });
+    }
+  } catch (error) {
+    console.error('Error during authentication', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.post('/products', checkAuthenticated, async (req, res) => {
+  const { token } = req.user;
+
+  try {
+    const response = await fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    const result = await response.json();
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.put('/products/:id', checkAuthenticated, async (req, res) => {
+  const { id } = req.params;
+  const { token } = req.user;
+
+  try {
+    const response = await fetch(`https://dummyjson.com/products/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    const result = await response.json();
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = { app };
---
+++ server/server.js
@@ -0,0 +1,4 @@
+const { app } = require('./index.js');
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
